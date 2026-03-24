86df35a2a4a5d5d75a68d983e2e2236eab58f32a
Supal Vasani <supalvasani@gmail.com>
2026-03-24 10:03:55 +0530

    backend db coonfig

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_, res) => res.send("API running"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server on ${PORT}`));
